537e0125b4bef474fcdb2104b1252412e413c833
Álan Cruz <alan_mugiwara@hotmail.com>
2026-05-07 18:47:52 -0300

    Implementação do algoritimo

---
+++ src/main.py
@@ -0,0 +1,23 @@
+print ('=================================\n'
+'Boas vindas ao Jobóia Rasp Quiz!\n'
+'=================================\n\n'
+'===================================================\n'
+'É um jogo que funciona totalmente via texto;\n'
+'Foi criado com a linguagem de programação Python;\n'
+'E combina programação com Hardware;\n'
+'Tudo o que você faz no jogo gera respostas físicas;\n'
+'Você vai ouvir sons de BIP e luzes piscando!\n'
+'===================================================\n')
+
+print('Regras do jogo:\n'
+'-> Serão feitas 10 perguntas;\n'
+'-> Cada pergunta possui apenas uma resposta certa;\n'
+'-> Cada resposta certa vale 10 pontos;\n'
+'-> Você deverá responder as perguntas digitando uma das letras (a, b, c ou d);\n'
+'-> Em caso de resposta >certa< o led verde vai piscar e serão tocados vários sons de bip;\n'
+'-> Em caso de resposta >errada< o led vermelho vai piscar e será tocado um longo som de bip;\n'
+'-> Respostas certas valem 10 pontos;\n'
+'-> Respostas erraas valem 0 pontos;\n'
+'-> O jogo irá calcular sua média no final;\n'
+'-> Divirta-se!')
+
